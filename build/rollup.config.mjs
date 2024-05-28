9663d43393077e9deeaf34315db4f6fb770c6dd3
jiamengda <jiamengda@jyd.com.cn>
2024-05-28 10:10:37 +0800

    feat: 修改发布配置&打包配置

--- build/rollup.config.mjs
+++ build/rollup.config.mjs
@@ -7,21 +7,21 @@ import { terser } from 'rollup-plugin-terser'; // Minify a bundle using Terser.
 export default {
     input: 'src/index.ts',
     output: [
-        {
-            file: 'dist/index.esm.js', // package.json 中 "module": "dist/index.esm.js"
-            format: 'esm', // es module 形式的包， 用来import 导入， 可以tree shaking
-            sourcemap: true
-        }, 
-        {
-            file: 'dist/index.cjs.js', // package.json 中 "main": "dist/index.cjs.js",
-            format: 'cjs', // commonjs 形式的包， require 导入 
-            sourcemap: true
-        }, 
+        // {
+        //     file: 'dist/index.esm.js', // package.json 中 "module": "dist/index.esm.js"
+        //     format: 'esm', // es module 形式的包， 用来import 导入， 可以tree shaking
+        //     sourcemap: true
+        // }, 
+        // {
+        //     file: 'dist/index.cjs.js', // package.json 中 "main": "dist/index.cjs.js",
+        //     format: 'cjs', // commonjs 形式的包， require 导入 
+        //     sourcemap: true
+        // }, 
         {
             file: 'dist/index.umd.js',
             name: 'GLWidget',
             format: 'umd', // umd 兼容形式的包， 可以直接应用于网页 script
-            sourcemap: true
+            // sourcemap: true
         }
     ],
     plugins:[
